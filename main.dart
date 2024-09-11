2f06600e2b6435b0de89755dedbb950c1d91793f
Yusra272 <yusraahsankhan1@gmail.com>
2024-09-11 06:45:44 -0700

    assi4

---
+++ main.dart
@@ -0,0 +1,58 @@
+void main(){
+  // Q1
+
+//   Map car = {
+//     'Brand': 'Toyota',
+//     'Color': 'Red',
+//     'isSedan': 'True'
+//   };
+   
+//    if(car ['isSedan'] == true && car ['color'] == 'Red'){
+//   print('Match');
+//    }
+
+//    else {
+//     print('No match');
+//    }
+
+//   //  Q2
+
+//   Map user = {
+//     'Name': 'Yusra',
+//     'isAdmin': 'false',
+//     'isActive': 'false'
+//   };
+
+//   if(user ['isAdmin'] == 'false' && ['isActive'] == 'false'){
+// print('Active admin');
+//   }
+
+// else{
+//   print('Not an active');
+// }
+
+
+// // Q3
+
+// for (var num = 1; num<= 10; num++)
+// print(num * 5);
+  
+
+  // Q4
+
+//     List<int> numbers = [3, 9, 1, 6, 4, 2, 8, 5, 7];
+
+// int largest = numbers[3];
+
+//   for (int number in numbers) {
+//     if (number > largest) {
+//       largest = number;
+//     }
+//   }
+
+//   // Print the largest element
+//   print('Largest element: $largest');
+  
+
+
+}
